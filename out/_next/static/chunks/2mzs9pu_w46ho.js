02d16fe708d6a0aee2dcd087d2c534943836c6db
Larfi44 <yaroslav.krasulya@gmail.com>
2026-08-31 17:06:35 +0300

    Build site for GitVerse Pages

---
+++ out/_next/static/chunks/2mzs9pu_w46ho.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,15511,t=>{t.v(e=>Promise.all(["static/chunks/2teo8l7pu5ze1.js"].map(e=>t.l(e))).then(()=>e(57349)))}]);
